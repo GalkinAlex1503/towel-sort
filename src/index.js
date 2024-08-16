aa277e4fe1c13ba75499cb03ff6bad4599b5f436
GalkinAlex1503 <galkin.alexandr1503@gmail.com>
2024-08-16 19:34:14 +0200

    feat: fis index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const result = [];
+  if (matrix === undefined) return [];
+    if (matrix === null) return [];
+  for (let i = 0; i < matrix.length; i++) {
+      let line = matrix[i];
+      if (i%2 === 1)    line.reverse();
+      for (let x of line) {
+          result.push(x);
+      }
+  }
+  return result;
 }
